05f3715fa07d7cad317b072b810e2e8e5b5e3aaf
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-05 14:08:42 +0600

    add sign out functionality and add some style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
 import { useState } from 'react';
 
@@ -8,8 +8,10 @@ const auth = getAuth(app)
 function App() {
   const [user, setUser] = useState({});
 
+  // providers
   const googleProvider = new GoogleAuthProvider();
 
+  // sign in
   const handleGoogleSignIn = () => {
 
     signInWithPopup(auth, googleProvider)
@@ -23,12 +25,30 @@ function App() {
       })
   }
 
+  // sign out
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
-      <p>user name: {user.displayName}</p>
-      <p>user email: {user.email}</p>
-      <img src={user.photoURL} alt="user img" />
+
+
+      <button className='sign-in-button' onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button className='sign-out-button' onClick={handleSignOut}>Sign Out</button>
+
+      <div className='user-info'>
+        <p> name: {user.displayName}</p>
+        <p> email: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>
+
     </div>
   );
 }
